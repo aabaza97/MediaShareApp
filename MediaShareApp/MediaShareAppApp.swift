594dbc361559aebecd3f2e459961990f5850a694
aabaza97 <aabaza32@hotmail.com>
2024-11-16 14:51:04 +0200

    Initial Commit

---
+++ MediaShareApp/MediaShareAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediaShareAppApp.swift
+//  MediaShareApp
+//
+//  Created by Ahmed Abaza on 16/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct MediaShareAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
